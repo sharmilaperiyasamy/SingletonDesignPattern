bc36cb4891ef31eeff6e3fe650c4d0416d6fded8
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-08-12 20:18:15 +0530

    [Sharmila] Singleton Design Pattern

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeachaer = Singleton.GetInstance;
+            fromTeachaer.PrintDetails("From Teacher");
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+
+    //No Thread-Safe Singleton Design Pattern implementation in C#
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
